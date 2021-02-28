23e026e2331dd2af622a28c5ece8cca0b41744ea
Maria Dolores <69127829+karlistack@users.noreply.github.com>
2021-02-28 18:00:28 +0100

    Delete taller_bicis directory

--- taller_bicis/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-	'name' : 'Taller_Bicis',
-	'description' : 'Gestor de una tienda de bicicletas, con funcion taller, tienda, administracion de la clientela etc',
-	'author' : 'Maria Dolores',
-	'depends' : [],
-	'application' : True,
-	'data' : ['view/bici.xml','view/report.xml']
-}
--- taller_bicis/model/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import bici,cliente,servicio
