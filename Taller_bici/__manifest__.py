5835a15971065e6bcb902de5a20c94e48cabd0e7
Maria Dolores <69127829+karlistack@users.noreply.github.com>
2021-02-28 18:09:58 +0100

    Add files via upload

---
+++ Taller_bici/__manifest__.py
@@ -0,0 +1,8 @@
+{
+	'name' : 'Taller_bici',
+	'description' : 'taller de bicis',
+	'author' : 'Maria Dolores Adamuz Barranco',
+	'depends' : [],
+	'application' : True,
+	'data' : ['view/Bici.xml','view/report.xml']
+}
