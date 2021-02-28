322dc7fafa014e72297a1fb9d71300751cb4891f
Maria Dolores <69127829+karlistack@users.noreply.github.com>
2021-02-28 19:42:08 +0100

    Delete Taller_bici directory

--- Taller_bici/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-	'name' : 'Taller_bici',
-	'description' : 'taller de bicis',
-	'author' : 'Maria Dolores Adamuz Barranco',
-	'depends' : [],
-	'application' : True,
-	'data' : ['view/Bici.xml','view/report.xml']
-}
